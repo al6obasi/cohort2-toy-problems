f94ee5fdd21828b0ecd22132eaa8f0fa6068c972
al6obasi <maltobasi@hotmail.com>
2017-01-19 09:10:53 +0200

    Linked List Soluation

---
+++ linkedList/linkedList.js
@@ -0,0 +1,69 @@
+
+function LinkedList(){
+	this.head = null;
+	this.tail = null;
+}
+
+LinkedList.prototype.IsEmpty = function() {
+	return this.head === null ;
+};
+
+LinkedList.prototype.size = function() {
+	var current = this.head;
+	var count = 0;
+
+	while(current !== null ){
+		count ++;
+		current = current.next;
+	}
+
+	return count ;
+};
+LinkedList.prototype.addToTail = function (val){
+	var newNode = {
+		value : val ,
+		next : null
+	};
+	if (this.IsEmpty()) {
+		this.head = newNode ;
+		return ;
+	}
+    var current = this.head;
+
+    while (current.next !== null){
+    	current = current.next;
+    }
+
+    current.next = newNode;
+};
+
+LinkedList.prototype.contains = function(val){
+	var current = this.head;
+
+	while (current !== null){
+		if (current.value === val) {
+			return true;
+		}
+		current = current.next;
+	}
+	return false ;
+};
+LinkedList.prototype.remove = function (val){
+	if (!this.contains(val)) {
+		return;
+	}
+	if (this.head.value === val) {
+		this.head = this.head.next;
+		return ;
+	}
+	var prev = null;
+	var curr = this.next;
+
+	while (curr.value !== val){
+		prev = curr;
+		curr = curr.next;
+	}
+	prev.next = curr.next;
+
+};
+//The complixity time for this linked list = O(log N)
