b0675935ca817a218e12a53f784e20f6eaf73df1
al6obasi <maltobasi@hotmail.com>
2017-02-02 09:13:37 +0200

    solve

---
+++ isSubsetOf/isSubsetOf.js
@@ -0,0 +1,36 @@
+/*
+ * Make an array method that can return whether or not a context array is a
+ * subset of an input array.  To simplify the problem, you can assume that neither
+ * array will contain objects or arrays as elements within them.
+ *
+ *
+ * var a = ['commit','push']
+ * a.isSubsetOf(['commit','rebase','push','blame']) // true
+ *
+ * NOTE: You should disregard duplicates in the set.
+ *
+ * var b = ['merge','reset','reset']
+ *
+ * b.isSubsetOf(['reset','merge','add','commit']) // true
+ *
+ * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
+ * subset.
+*/
+
+
+/*
+ * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
+*/
+Array.prototype.isSubsetOf = function (arr) {
+  var count = 0; 
+  // console.log(this)
+  for (var i = 0; i < this.length; i++) {
+  	for (var j = 0; j < arr.length; j++) {
+  		if (arr[j] === this[i]) {
+  			count ++ ;
+  		}
+  	}
+  }
+    return count === this.length;
+};
+// the time complixity is O(n^2)
